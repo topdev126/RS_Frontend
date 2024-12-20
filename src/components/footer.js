b4560e81df103b2097b448dac019effbce37d09c
RF9862 <que.quao126@gmail.com>
2024-12-20 05:07:34 +0100

    fix contact

--- src/components/footer.js
+++ src/components/footer.js
@@ -147,7 +147,7 @@ export default function Footer() {
                       <FiMapPin className="fea icon-sm text-primary mt-1 me-3" />
                       <div className="">
                         <p className="mb-2">
-                          Singapore
+                          Serbia
                         </p>
                         <Link
                           to="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39206.002432144705!2d-95.4973981212445!3d29.709510002925988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640c16de81f3ca5%3A0xf43e0b60ae539ac9!2sGerald+D.+Hines+Waterwall+Park!5e0!3m2!1sen!2sin!4v1566305861440!5m2!1sen!2sin"
@@ -162,10 +162,10 @@ export default function Footer() {
                     <div className="d-flex mt-4">
                       <FiMail className="fea icon-sm text-primary mt-1 me-3" />
                       <Link
-                        to="mailto:judaar@gmail.com "
+                        to="mailto:strongsteel222@gmail.com "
                         className="text-foot"
                       >
-                        judaar@gmail.com 
+                        strongsteel222@gmail.com 
                       </Link>
                     </div>
 
--- src/pages/contactus.js
+++ src/pages/contactus.js
@@ -182,10 +182,10 @@ export default function ContactUs() {
                     Start working with Pro-2 that can provide everything
                   </p>
                   <Link
-                    to="mailto:judaar@gmail.com "
+                    to="mailto:strongsteel222@gmail.com "
                     className="text-primary"
                   >
-                    judaar@gmail.com 
+                    strongsteel222@gmail.com 
                   </Link>
                 </div>
               </div>
@@ -203,7 +203,7 @@ export default function ContactUs() {
                 <div className="mt-4">
                   <h5 className="mb-3">Location</h5>
                   <p className="text-muted">
-                    Singapore
+                    Serbia
                   </p>
                   <Link
                     to="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39206.002432144705!2d-95.4973981212445!3d29.709510002925988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640c16de81f3ca5%3A0xf43e0b60ae539ac9!2sGerald+D.+Hines+Waterwall+Park!5e0!3m2!1sen!2sin!4v1566305861440!5m2!1sen!2sin"
